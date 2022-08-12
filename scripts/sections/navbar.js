0045245ee4daedaf356f531993b91a8d23239d83
cancle54 <103057044+cancle54@users.noreply.github.com>
2022-08-11 18:28:29 -0700

    Update navbar.js

--- scripts/sections/navbar.js
+++ scripts/sections/navbar.js
@@ -1,6 +1,8 @@
 const fragment = new URLSearchParams(window.location.hash.slice(1));
 const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
 
+console.log(window.location.hash);
+
 console.log(accessToken);
 console.log(tokenType);
 
