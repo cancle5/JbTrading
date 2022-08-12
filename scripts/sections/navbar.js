9b7294912e88843e1fd3afa9a3b019389d8fcdaa
cancle54 <103057044+cancle54@users.noreply.github.com>
2022-08-12 08:49:34 -0700

    Fixtry

--- scripts/sections/navbar.js
+++ scripts/sections/navbar.js
@@ -1,64 +1,10 @@
-window.onload = () => {
+fetch("/sections/navbar.html")
+.then((response) => response.text())
+.then((html) => {
 
-    const fragment = new URLSearchParams(window.location.hash.slice(1));
-    const [accessToken, tokenType, code] = [fragment.get('access_token'), fragment.get('token_type'), fragment.get('code')];
+    document.getElementById("navbar").innerHTML = html;
 
-    console.log(accessToken);
-    console.log(tokenType);
-    console.log(code);
-
-    fetch("/sections/navbar.html")
-    .then((response) => response.text())
-    .then((html) => {
-
-        document.getElementById("navbar").innerHTML = html;
-
-        if (localStorage["SiteToken"]) {
-            if (localStorage["SiteTokenType"]) {
-
-                console.log("Logged In | Cookie");
-        
-                document.getElementById("navbar-textbutton").innerText = "Logged In";
-
-            };
-        };
-
-        if (accessToken) {
-
-            console.log("Logged In | Discord");
-
-            localStorage["SiteToken"] = accessToken;
-
-            localStorage["SiteTokenType"] = tokenType;
-
-            document.getElementById("navbar-textbutton").innerText = "Logged In";
-
-            window.location.href = window.location.href.split('?')[0];
-
-        };
-
-    })
-    .catch((error) => {
-        console.warn(error);
-    });
-
-}    
-
-/*
-fetch('https://discord.com/api/users/@me', {
-headers: {
-    authorization: `${tokenType} ${accessToken}`,
-},
 })
-.then(result => result.json())
-.then(response => {
-    console.log(response);
-    const { username, discriminator, avatar, id} = response;
-    //set the welcome username string
-    document.getElementById('name').innerText = ` ${username}#${discriminator}`;
-
-    //set the avatar image by constructing a url to access discord's cdn
-    document.getElementById("avatar").src = `https://cdn.discordapp.com/avatars/${id}/${avatar}.jpg`;
-})
-.catch(console.error);
-*/
+.catch((error) => {
+    console.warn(error);
+});
