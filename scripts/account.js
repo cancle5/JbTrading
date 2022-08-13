b1292c6955ebf5ad0b4227965fbee2930d47662a
cancle54 <103057044+cancle54@users.noreply.github.com>
2022-08-13 09:30:43 -0700

    Google oauth

--- scripts/account.js
+++ scripts/account.js
@@ -10,20 +10,8 @@ function handleCredentialResponse(response) {
 
 window.onload = function () {
     
-    google.accounts.id.initialize({
-        client_id: "505525286631-ikfvhpi1up7abec6vbd97mb840qgc43e.apps.googleusercontent.com",
-        callback: handleCredentialResponse,
-        auto_select: true,
-        auto: true
-    });
-    
-}
+    if (localStorage["AccountInformation"]) {
 
-function decodeJwtResponse(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return JSON.parse(jsonPayload);
+    }
+    
 }
