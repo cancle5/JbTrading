c79c3ac27d3cdc5fc6871d3e12f37d6f67fd6f16
cancle54 <103057044+cancle54@users.noreply.github.com>
2022-08-14 11:36:50 -0700

    Progress

--- scripts/tradefinder.js
+++ scripts/tradefinder.js
@@ -1,3 +1,79 @@
+let Data = [];
+
+var popup = document.getElementById('popup');
+var itemList = document.getElementById('itempopuplist');
+
+var create_ad_items = document.document.getElementById('create-ad-main').getElementsByClassName('box-trade-ad-item');
+
+const displayItems = (characters) => {
+
+    const htmlString = characters
+        .map((character) => {
+
+            return `
+            <div id="${character.ID}" class="trade-selectionbox">
+
+                <div id="${character.Header1}" class="text" style="font-size: 1.8rem; font-weight: 600;">${character.Header1}</div>
+                <div style="padding-top: 1rem"></div>
+                <img id="trade-selectionimage" src="${character.Image1}" />
+
+            </div>
+        `;
+        })
+    .join(''); 
+
+    itemList.innerHTML = itemList.innerHTML + htmlString
+
+    for (const item of itemList.getElementsByClassName('trade-selectionbox')){
+    
+        item.addEventListener('click', (event) => {
+
+
+    
+            popup.style.display = 'none';
+    
+        });
+    
+    }
+
+};
+
+const loadData = async () => {
+
+    try {
+
+        await fetch('https://opensheet.elk.sh/1T9-q3RWbYz_m9g5RPygdAC4vod09XJIRUcN0ijkuSts/1')
+        .then(result => result.json())
+        .then((output) => {
+    
+            console.log(output);
+
+            Data = output;
+    
+            displayItems(output);
+    
+            localStorage["DataBackup"] = JSON.stringify(output);
+    
+        })
+
+    } catch (error) {
+
+        console.log(error);
+
+        Data = JSON.parse(localStorage["DataBackup"]);
+
+        console.log(Data);
+    
+        if (Array.isArray(Data)) {
+    
+            displayItems(Data);
+
+        }
+
+    }
+
+};
+
 window.onload = function () {
 
     var Information = JSON.parse(localStorage["AccountInformation"]);
@@ -19,5 +95,17 @@ window.onload = function () {
         })
 
     };
+
+    loadData();
+
+    for (const item of create_ad_items){
+    
+        item.addEventListener('click', (event) => {
+    
+            popup.style.display = 'block';
+    
+        });
+    
+    }
     
 };
