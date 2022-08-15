8640888899a2145e5699780bfc531dce1c2417c7
cancle54 <103057044+cancle54@users.noreply.github.com>
2022-08-14 18:30:25 -0700

    Update tradefinder.js

--- scripts/tradefinder.js
+++ scripts/tradefinder.js
@@ -5,26 +5,22 @@ var itemList = document.getElementById('itempopuplist');
 
 var selected = null;
 
-window.onload = function () {
-
-    var Information = JSON.parse(localStorage["AccountInformation"]);
+var Information = JSON.parse(localStorage["AccountInformation"]);
     
-    if (Information === null || Information === undefined) {
+if (Information === null || Information === undefined) {
 
-        console.log("ERROR | No login data found.");
+    console.log("ERROR | No login data found.");
 
-    } else {
+} else {
 
-        fetch("/sections/create-ad.html")
-        .then((response) => response.text())
-        .then((html) => {
-    
-            document.getElementById("box-create-ad-7p979z8r").innerHTML = html;
+    fetch("/sections/create-ad.html")
+    .then((response) => response.text())
+    .then((html) => {
 
-            document.getElementById("pfp-create-ad").style.backgroundImage = Information.picture;
-    
-        })
+        document.getElementById("box-create-ad-7p979z8r").innerHTML = html;
 
-    };
-    
+        document.getElementById("pfp-create-ad").style.backgroundImage = Information.picture;
+
+    })
+
 };
