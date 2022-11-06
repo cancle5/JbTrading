d5c56a7c2736227ab071aaf5f92f36639efbdd30
cancle54 <103057044+cancle54@users.noreply.github.com>
2022-11-05 18:06:05 -0700

    fix

--- scripts/scrollanimation.js
+++ scripts/scrollanimation.js
@@ -7,91 +7,101 @@ function isInViewport(element) {
 }
 
 window.onload = function(){
-    
-    const navbar = document.getElementById("navbar");
-    const footer = document.getElementById("footer");
 
-    const faders = document.querySelectorAll(".fade-in");
-    const fromleft = document.querySelectorAll(".from-left");
-    const fromRight = document.querySelectorAll(".from-right");
-    
-    var scrolltop = true;
-    var transitioning = false;
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
 
-    function animateItems() {
+        console.log("Mobile device. Not loading animations.");
 
-        faders.forEach(fader => {
-    
-            if (isInViewport(fader)) {
-                if (fader.classList.contains('appear') == false) {
-                    fader.classList.add("appear");
+    } else {
+
+        console.log("Loading animations.");
+
+        const navbar = document.getElementById("navbar");
+        const footer = document.getElementById("footer");
+
+        const faders = document.querySelectorAll(".fade-in");
+        const fromleft = document.querySelectorAll(".from-left");
+        const fromRight = document.querySelectorAll(".from-right");
+        
+        var scrolltop = true;
+        var transitioning = false;
+
+        function animateItems() {
+
+            faders.forEach(fader => {
+        
+                if (isInViewport(fader)) {
+                    if (fader.classList.contains('appear') == false) {
+                        fader.classList.add("appear");
+                    }
+                } else {
+                    if (fader.classList.contains('appear') == true) {
+                        fader.classList.remove("appear");
+                    }
                 }
-            } else {
-                if (fader.classList.contains('appear') == true) {
-                    fader.classList.remove("appear");
+        
+            });
+
+            fromleft.forEach(box => {
+        
+                if (isInViewport(box)) {
+                    if (!box.classList.contains('appear')) {
+                        box.classList.add("appear");
+                    }
+                } else {
+                    if (box.classList.contains('appear')) {
+                        box.classList.remove("appear");
+                    }
                 }
+        
+            });
+
+            fromRight.forEach(box => {
+        
+                if (isInViewport(box)) {
+                    if (!box.classList.contains('appear')) {
+                        box.classList.add("appear");
+                    }
+                } else {
+                    if (box.classList.contains('appear')) {
+                        box.classList.remove("appear");
+                    }
+                }
+        
+            });
+        
+        }
+
+        setInterval(function() { 
+
+            animateItems();
+
+            if (window.scrollY > 100) {
+
+                if (scrolltop == true) {
+
+                    scrolltop = false;
+
+                    navbar.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+                    navbar.style.transition = "all 1s";
+
+                }  
+                
             }
-    
-        });
+            else {
 
-        fromleft.forEach(box => {
-    
-            if (isInViewport(box)) {
-                if (!box.classList.contains('appear')) {
-                    box.classList.add("appear");
-                }
-            } else {
-                if (box.classList.contains('appear')) {
-                    box.classList.remove("appear");
-                }
-            }
-    
-        });
+                if (scrolltop == false) {
 
-        fromRight.forEach(box => {
-    
-            if (isInViewport(box)) {
-                if (!box.classList.contains('appear')) {
-                    box.classList.add("appear");
-                }
-            } else {
-                if (box.classList.contains('appear')) {
-                    box.classList.remove("appear");
-                }
+                    scrolltop = true;
+
+                    navbar.style.backgroundColor = "rgba(9, 13, 23)";
+                    navbar.style.transition = "all 1s";
+
+                }   
             }
-    
-        });
-    
+
+        }, 100);
+
     }
-
-    setInterval(function() { 
-
-        animateItems();
-
-        if (window.scrollY > 100) {
-
-            if (scrolltop == true) {
-
-                scrolltop = false;
-
-                navbar.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
-                navbar.style.transition = "all 1s";
-
-            }  
-            
-        }
-        else {
-
-            if (scrolltop == false) {
-
-                scrolltop = true;
-
-                navbar.style.backgroundColor = "rgba(9, 13, 23)";
-                navbar.style.transition = "all 1s";
-
-            }   
-        }
-
-     }, 500);
 
 };
