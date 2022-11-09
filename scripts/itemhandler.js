a0cd562964456003115d75850bd7af426ebc0a69
cancle54 <103057044+cancle54@users.noreply.github.com>
2022-11-09 13:30:08 -0800

    htaccess

--- scripts/itemhandler.js
+++ scripts/itemhandler.js
@@ -1,99 +1,109 @@
-const Header1 = document.getElementById("Header1");
-const Text1 = document.getElementById("Text");
-const Image1 = document.getElementById("Image");
+var interval = setInterval(function() {
 
-const Value = document.getElementById("Value");
-const Price = document.getElementById("Price");
-const MaxSpeed = document.getElementById("MaxSpeed");
-const Rarity = document.getElementById("Rarity");
-const Demand = document.getElementById("Demand");
+    if ( localStorage[DataSaveName] !== undefined ){
 
-const ItemCategory = document.getElementById("ItemCategory").getAttribute("value");
-const ItemName = document.getElementById("ItemNumber").getAttribute("value");
+        clearInterval(interval);
 
-const MetaTags = document.head.getElementsByTagName("meta");
+        const Header1 = document.getElementById("Header1");
+        const Text1 = document.getElementById("Text");
+        const Image1 = document.getElementById("Image");
 
-console.log(DataSaveName);
+        const Value = document.getElementById("Value");
+        const Price = document.getElementById("Price");
+        const MaxSpeed = document.getElementById("MaxSpeed");
+        const Rarity = document.getElementById("Rarity");
+        const Demand = document.getElementById("Demand");
 
-const Data = JSON.parse(localStorage[DataSaveName]);
-const ItemData = Data[ItemCategory][ItemName];
+        const ItemCategory = document.getElementById("ItemCategory").getAttribute("value");
+        const ItemName = document.getElementById("ItemNumber").getAttribute("value");
 
-console.log(ItemData);
+        const MetaTags = document.head.getElementsByTagName("meta");
 
-const displayChart = () => {
+        console.log(DataSaveName);
 
-    var valuehistorydates = ItemData["valuehistorydate"].split(" ");
-    var valuehistory = ItemData["valuehistory"].split(" ");
+        const Data = JSON.parse(localStorage[DataSaveName]);
+        const ItemData = Data[ItemCategory][ItemName];
 
-    var ctx = document.getElementById("myChart").getContext("2d");
+        console.log(ItemData);
 
-    var config = {
-        type: "line",
-        data: {
-            labels: valuehistorydates,
-            datasets: [{
-                label: "Value",
-                data: valuehistory,
-                backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-                borderColor: ["rgba(255, 99, 132, 1)"],
-                borderWidth: 1,
-            }, ],
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: false,
+        const displayChart = () => {
+
+            var valuehistorydates = ItemData["valuehistorydate"].split(" ");
+            var valuehistory = ItemData["valuehistory"].split(" ");
+
+            var ctx = document.getElementById("myChart").getContext("2d");
+
+            var config = {
+                type: "line",
+                data: {
+                    labels: valuehistorydates,
+                    datasets: [{
+                        label: "Value",
+                        data: valuehistory,
+                        backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+                        borderColor: ["rgba(255, 99, 132, 1)"],
+                        borderWidth: 1,
+                    }, ],
                 },
-            },
-        },
-    };
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: false,
+                        },
+                    },
+                },
+            };
 
-    var myChart = new Chart(ctx, config);
-    
-};
+            var myChart = new Chart(ctx, config);
+            
+        };
 
-if (ItemData != null && ItemData != undefined) {
+        if (ItemData != null && ItemData != undefined) {
 
-    Header1.innerHTML = ItemData["name"];
-    Text1.innerHTML = ItemData["description"];
-    Image1.innerHTML = '<img src="' + ItemData["image"] + '" alt="' + ItemData["name"] + '"></img>';
+            Header1.innerHTML = ItemData["name"];
+            Text1.innerHTML = ItemData["description"];
+            Image1.innerHTML = '<img src="' + ItemData["image"] + '" alt="' + ItemData["name"] + '"></img>';
 
-    Value.innerHTML = "Value: " + ItemData["value"];
-    Price.innerHTML = "Price: " + ItemData["price"];
-    MaxSpeed.innerHTML = "Max Speed: " + ItemData["maxspeed"];
-    Demand.innerHTML = "Demand: " + ItemData["demand"];
-    Rarity.innerHTML = "Rarity: " + ItemData["rarity"];
+            Value.innerHTML = "Value: " + ItemData["value"];
+            Price.innerHTML = "Price: " + ItemData["price"];
+            MaxSpeed.innerHTML = "Max Speed: " + ItemData["maxspeed"];
+            Demand.innerHTML = "Demand: " + ItemData["demand"];
+            Rarity.innerHTML = "Rarity: " + ItemData["rarity"];
 
-    displayChart();
+            displayChart();
 
-    document.getElementsByTagName("title")[0].innerHTML = "JB Values - " + ItemData["name"];
+            document.getElementsByTagName("title")[0].innerHTML = "JB Values - " + ItemData["name"];
 
-    for (var Tag in MetaTags) {
+            for (var Tag in MetaTags) {
 
-        Tag = MetaTags[Tag];
+                Tag = MetaTags[Tag];
 
-        if (Tag.name == "title") {
+                if (Tag.name == "title") {
 
-            Tag.content = "JB Values - " + ItemData["name"];
+                    Tag.content = "JB Values - " + ItemData["name"];
 
-        }
+                }
 
-        if (Tag.name == "description") {
+                if (Tag.name == "description") {
 
-            Tag.content = ItemData["description"];
+                    Tag.content = ItemData["description"];
 
-        }
+                }
 
-        if (Tag.name == "keywords") {
+                if (Tag.name == "keywords") {
 
-            Tag.content = "roblox jailbreak " + ItemData["name"];
+                    Tag.content = "roblox jailbreak " + ItemData["name"];
+
+                }
+
+            }    
+
+        } else {
+
+            console.warn("Failed to load item.");
 
         }
 
     }    
 
-} else {
-
-    console.warn("Failed to load item.");
-
-}
+ }, 100);
